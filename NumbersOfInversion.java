6cc5cba495c429c5deab9a3f59c03bacceef4c49
Sujith-Vel <sujith.vel63@gmail.com>
2025-04-23 18:14:05 +0530

    Create NumbersOfInversion.java

---
+++ NumbersOfInversion.java
@@ -0,0 +1,63 @@
+package DSA.Random;
+
+import java.util.Arrays;
+
+public class NumbersOfInversion {
+    public static void main(String[] args) {
+        System.out.println("ANS : " + Arrays.toString(mergeSort(new int[]{5,4,3,2,1})));
+    }
+
+    public static int[] mergeSort(int[] ar){
+
+        if(ar.length==1){
+            return ar;
+        }
+
+        int m = ar.length/2;
+        int[] f = mergeSort(Arrays.copyOfRange(ar,0,m));
+        int[] s = mergeSort(Arrays.copyOfRange(ar,m,ar.length));
+
+        return merge(f,s);
+    }
+
+    private static int[] merge(int[] f, int[] s) {
+
+        int count = 0;
+
+        int n = Math.max(f.length, s.length);
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        int[] ans = new int[f.length+s.length];
+
+        for (; i < n; i++) {
+            if(f[j]<s[k]){
+                count++;
+                ans[i] = f[j];
+                j++;
+            }else {
+                ans[i] = s[k];
+                k++;
+            }
+        }
+
+        while (j < f.length){
+            count++;
+            ans[i] = f[j];
+            j++;
+            i++;
+        }
+
+        while (k< s.length){
+            ans[i] = s[k];
+            k++;
+            i++;
+        }
+        System.out.println(count);
+
+        return ans;
+
+    }
+}
